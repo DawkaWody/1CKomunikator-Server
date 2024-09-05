a9a6e54d5dbc916928bac9032cbd0986ff9a6978
Wojciech <69240079+cyber-wojtek@users.noreply.github.com>
2024-09-05 21:57:12 +0200

    Switched to production WSGI server.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
-from flask import Flask
+import flask
+import waitress
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route("/")
 def hello_world() -> str:
     return "<p>Hello, World!</p><a href=/link>A linky link!</a>"
 
-@app.route("/link")
+@app.route("/fetchinfo")
 def link() -> str:
     return "<a href=\"/\">Such a clicky link!</a>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    waitress.serve(app, host="0.0.0.0", port="8000")
