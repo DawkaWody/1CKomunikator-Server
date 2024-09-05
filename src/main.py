51509466cf4a8a9fbd4ed3b2cad5cc25e8f68a83
Wojciech <69240079+cyber-wojtek@users.noreply.github.com>
2024-09-05 20:50:24 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,6 +9,6 @@ def hello_world():
 
 @app.route("/link")
 def link():
-    return f"<a href=\"{base64.urlsafe_b64decode(base64.urlsafe_b64decode(base64.urlsafe_b64decode(b'WVVoU01HTklUVFpNZVRsM1lrTTFkMkl6U25WaFNGWnBURzFPZG1KVE9EMD0='))).decode(encoding="utf-8")}\">Such a clicky link!</a>"
+    return "<a href=\"\\\">Such a clicky link!</a>"
 
 app.run("localhost", 2000)
