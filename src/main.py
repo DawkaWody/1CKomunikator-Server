638976093fe0914f025aec37cb76ef6ce1d2b7e0
Wojciech <69240079+cyber-wojtek@users.noreply.github.com>
2024-09-06 17:49:11 +0200

    Aktualizacja logowania.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,49 @@
-import flask
-import waitress
+from enum import Enum
 
-app = flask.Flask(__name__)
+import waitress
+from flask import Flask, request, session
+
+
+class LoginErrorReason(Enum):
+    invalid_username = 0,
+    invalid_password = 1,
+    other = 2
+
+
+class AccountCreationErrorReason(Enum):
+    invalid_password = 0,
+    user_already_exists = 1
+    other = 2
+
+
+app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world() -> str:
     return "<p>Hello, World!</p><a href=/link>A linky link!</a>"
 
-@app.route("/link")
-def link() -> str:
-    return "<a href=\"/\">Such a clicky link!</a>"
+
+@app.post("/login")
+def login() -> dict:
+    """
+    {"username", "password"}
+    :return:
+    """
+    # todo: add hashing function
+    username = request.form["username"]
+    input_password = request.form["password"]
+    # password = get_user(username)
+    password = ["admin"]  # tmp
+    # if not verify_password(input_password, password):
+    #    return {
+    #        "success": False,
+    #        "reason": LoginErrorReason.invalid_password
+    #    }
+    # else:
+    return {
+        "success": True,
+    }
 
 app.config.from_mapping(
     DATABASE="./main_db.sqlite",
