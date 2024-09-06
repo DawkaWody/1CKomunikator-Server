2488a8a6ceec5d004d2fedc458d1ee7a89c24b58
Wojciech <69240079+cyber-wojtek@users.noreply.github.com>
2024-09-06 17:28:35 +0200

    .

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world() -> str:
     return "<p>Hello, World!</p><a href=/link>A linky link!</a>"
-
+#
 
 @app.post("/login")
 def login() -> dict:
