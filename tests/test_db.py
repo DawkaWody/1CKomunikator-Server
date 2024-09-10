57d60755622692a8636ba08b15b3423f35e8ffee
Wojciech <69240079+cyber-wojtek@users.noreply.github.com>
2024-09-10 17:09:27 +0200

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,12 +4,12 @@ try:
 except (ImportError, ModuleNotFoundError) as err:
     msg = "Please install testing requirements (pyptest and requests)"
     raise TypeError(msg) from err
+import collections
+import pathlib
 import shutil
 import sqlite3
-import uuid
-import pathlib
-import collections
 import typing
+import uuid
 
 import db
 from db import DbManager
